b862757dd0f27400fc41362568ad1a2ff78784a2
eduardo-gabriel-frainer <eduardogfrainer@gmail.com>
2025-02-21 14:08:37 -0300

    testes

---
+++ src/test/java/com/example/vencimento_criptoativos/VencimentoCriptoativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vencimento_criptoativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VencimentoCriptoativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
